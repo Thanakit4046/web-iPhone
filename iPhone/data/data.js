946e26f401148d243378695264ae329cca819cc3
Thanakit4046 <thanakit4046@gmail.com>
2023-08-24 13:09:21 +0700

    ล่าสุด

--- iPhone/data/data.js
+++ iPhone/data/data.js
@@ -1,78 +1,128 @@
 var product = [{
     "id": 0,
     "title": "iPhone 14 Pro max",
-    "price": "",
+    "price": "35900",
     "description": "จอภาพ Super Retina XDR ขนาด 6.1 นิ้ว แบตเตอรี่ที่ใช้งานได้ตลอดวันและเล่นวิดีโอได้นานสูงสุด 20 ชั่วโมง ชิป A15 Bionic พร้อม GPU แบบ 5-core เพื่อประสิทธิภาพที่เร็วสุดขั้ว ระบบเซลลูลาร์ 5G ที่เร็วสุดแรง ระบบกล้องสุดล้ำเพื่อภาพถ่ายที่ดียิ่งขึ้นในทุกสภาพแสงiOS 16 มาพร้อมวิธีที่หลากหลายยิ่งกว่าเดิม",
-    "brand": "",
-    "image": ""
+    "brand": "iPhone",
+    "image": ""//รูป
 }, {
     "id": 1,
-    "title": "Samsung Galaxy S22",
-    "price": 179999,
-    "description": "จอภาพ Super Retina XDR ขนาด 6.1 นิ้วDynamic Island วิธีใหม่ที่มหัศจรรย์ในการโต้ตอบกับ iPhoneกล้องหลักความละเอียด 48MP ที่มีความละเอียดมากขึ้นสูงสุด 4 เท่าโหมดภาพยนตร์ที่วันนี้มาในแบบ Dolby Vision ระดับ 4K สูงสุด 30 fpsโหมดแอ็คชั่นเพื่อวิดีโอแบบถือถ่ายที่นิ่งและไหลลื่A16 Bionic ที่สุดแห่งชิปสมาร์ทโฟน ระบบเซลลูลาร์ 5G ที่เร็วสุด",
-    "brand": "Samsung ",
-    "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyS22-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 2,
-    "title": "Vivo X80",
-    "price": 159999,
-    "description": "Vivo X80 price in Pakistan is Rs. 159,999. Official dealers and warranty providers regulate the retail price of Vivo mobile products in official warranty.",
-    "brand": "Vivo",
-    "image": "https://www.whatmobile.com.pk/admin/images/Vivo/VivoX80-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 3,
-    "title": "Oppo F21 Pro 5G",
-    "price": 69999,
-    "description": "Oppo F21 Pro 5G price in Pakistan is Rs. 69,999. Official dealers and warranty providers regulate the retail price of Oppo mobile products in official warranty.",
-    "brand": "Oppo",
-    "image": "https://www.whatmobile.com.pk/admin/images/Oppo/OppoF21Pro5G-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 4,
-    "title": "Infinix Note 12",
-    "price": 36999,
-    "description": "Infinix Note 12 price in Pakistan is Rs. 36,999. Official dealers and warranty providers regulate the retail price of Infinix mobile products in official warranty.",
-    "brand": "Infinix",
-    "image": "https://www.whatmobile.com.pk/admin/images/Infinix/InfinixNote12-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 5,
-    "title": "Samsung Galaxy A13",
-    "price": 35499,
-    "description": "Samsung Galaxy A13 price in Pakistan is Rs. 35,499. Official dealers and warranty providers regulate the retail price of Samsung mobile products in official warranty.",
-    "brand": "Samsung",
-    "image": "https://www.whatmobile.com.pk/admin/images/Samsung/SamsungGalaxyA13-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 6,
-    "title": "Xiaomi 12",
-    "price": 179999,
-    "description": "Xiaomi 12 price in Pakistan is Rs. 179,999. Official dealers and warranty providers regulate the retail price of Xiaomi mobile products in official warranty.",
-    "brand": "Xiaomi",
-    "image": "https://www.whatmobile.com.pk/admin/images/Xiaomi/Xiaomi12-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 7,
-    "title": "Huawei Nova 9",
-    "price": 107999,
-    "description": "Huawei Nova 9 price in Pakistan is Rs. 107,999. Official dealers and warranty providers regulate the retail price of Huawei mobile products in official warranty.",
-    "brand": "Huawei",
-    "image": "https://www.whatmobile.com.pk/admin/images/Huawei/HuaweiNova9-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 8,
-    "title": "Realme C35",
-    "price": 32999,
-    "description": "Realme C35 price in Pakistan is Rs. 32,999. Official dealers and warranty providers regulate the retail price of Realme mobile products in official warranty.",
-    "brand": "Realme",
-    "image": "https://www.whatmobile.com.pk/admin/images/Realme/RealmeC35-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 9,
-    "title": "Tecno Spark 8C 3GB",
-    "price": 20499,
-    "description": "Tecno Spark 8C 3GB price in Pakistan is Rs. 20,499. Official dealers and warranty providers regulate the retail price of Tecno mobile products in official warranty.",
-    "brand": "Tecno",
-    "image": "https://www.whatmobile.com.pk/admin/images/Tecno/TecnoSpark8C3GB-b.jpg"
-}, {
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
     "id": 10,
-    "title": "itel Vision 3",
-    "price": 16299,
-    "description": "itel Vision 3 price in Pakistan is Rs. 16,299. Official dealers and warranty providers regulate the retail price of itel mobile products in official warranty.",
-    "brand": "Itel",
-    "image": "https://www.whatmobile.com.pk/admin/images/itel/itelVision3-b.jpg"
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 11,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 12,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 13,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 14,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 15,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 16,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
+},{
+    "id": 17,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "",
 }]
+    
